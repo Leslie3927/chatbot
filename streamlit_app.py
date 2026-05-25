0724d7411a3f12cbc7a978c7f2d2233b1de38090
Leslie3927 <1090282@lwsd.org>
2026-05-25 09:40:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,14 @@ with st.form("my_form"):
             "Poipole"
         ]
     )
+    client = OpenAI(
+    api_key = "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"
+)
     
 system_prompt = """
 You are to act like a Pokedex from the Pokemon franchise.
 
-A user will choose the name of a Pokemon.
+A user will submit the name of a Pokemon.
 
 If the request is for an official Pokemon, give them a description that matches
 the official Pokedex entry as closely as possible.
@@ -37,4 +40,4 @@ If you don't recognize the Pokemon, act as if it was a Pokemon and describe it.
 Return only valid JSON matching the required schema.
 """
 
-
+st.form_submit_button()
